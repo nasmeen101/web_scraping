c46c604b307e3e854ac8a4ec7ceeda880cfbe1bf
nasmeen101 <nasmeen000@gmail.com>
2023-06-28 22:17:11 +0700

    Revert "test"
    This reverts commit 6352047d60198c19d9b0aad5efe8e371d729b25a.

--- get_job.py
+++ get_job.py
@@ -57,6 +57,3 @@ def process_job(input_date):
     db.report.insert_one(report.copy())
     db.keyword.insert_one({"keyword": job[:]})
     return [report.copy(), {"keyword": job[:]}, job_setting[:]]
-
-
-############
